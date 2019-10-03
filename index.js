e741f5f15ac483b95a10e566c307d8203c68359f
PrinceK001 <tripathi.prince635@gmail.com>
2019-10-03 19:00:30 +0530

    New JavaScript beginner level questions added

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
 // Import stylesheets
 import "./style.css";
 
-// Write Javascript code!
 const appDiv = document.getElementById("app");
 appDiv.innerHTML = `
 <h1>Interview Questions - Preparation</h1>
 <ol>
   <li>Fizz, buzz and fizzbuzz</li>
-  <li>Fibonacci Series</li>
+  <li>Fibonacci series</li>
+  <li>Reverse a string</li>
+  <li>Palindrome</li>
 </ol>`;
 
-// 1st fizz, buzz and fizz buzz
-// const printData = { "3": "fizz", "5": "buzz" };
-// const boundaries = { start: 1, end: 30 };
-// const printer = (data, boundaryDecider) => {
-//   const keys = Object.keys(data);
-//   for (let i = boundaryDecider.start; i <= boundaryDecider.end; i++) {
-//     let str = "";
-//     keys.forEach((ele, index) => {
-//       if (i % ele == 0) {
-//         str += printData[ele];
-//       }
-//     });
-//     str ? console.log(str) : console.log("i = ", i);
-//   }
-// };
-// printer(printData, boundaries);
+// 1 fizz, buzz and fizz buzz
+const printData = { "3": "fizz", "5": "buzz" };
+const boundaries = { start: 1, end: 30 };
+const printer = (data, boundaryDecider) => {
+  const keys = Object.keys(data);
+  for (let i = boundaryDecider.start; i <= boundaryDecider.end; i++) {
+    let str = "";
+    keys.forEach((ele, index) => {
+      if (i % ele == 0) {
+        str += printData[ele];
+      }
+    });
+    str ? console.log(str) : console.log("i = ", i);
+  }
+};
+printer(printData, boundaries);
 
-// fibonacci series
+//2 fibonacci series
 let sum = 0, limit = 100 ;
 const fibonacciPrinter = (last, secondLast) => {
   sum = last + secondLast;
@@ -38,3 +39,22 @@ const fibonacciPrinter = (last, secondLast) => {
 }
 fibonacciPrinter(0, 1);
 
+//3 a. reverse a string
+const reverseItOne = (text) => {
+  return text.split('').reverse().join('');
+};
+console.log(reverseItOne('test!'));
+
+//3 b. reverse without reverse function
+const reverseIt = (text) => {
+  let str = '', i = text.length-1;
+  for(i; i>-1; i--){
+    str += text[i];
+  }
+  return str;
+}
+console.log(reverseIt('test!'));
+
+//4. can also develop palindrome function over it
+console.log('ollo' == reverseItOne('ollo'));
+console.log('test' == reverseItOne('test'));
